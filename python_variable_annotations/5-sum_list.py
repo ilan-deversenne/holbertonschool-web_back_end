8c48a860c7e22081c2443d265e7abe2433bf5f61
ilan-deversenne <ilan.deversenne@holbertonstudents.com>
2026-04-09 11:26:58 +0200

    fix: set type to list -> typing.LIst

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
+import typing
 
 """
 Function to sum a list of floats
 """
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: typing.List[float]) -> float:
     """
     Sums a list of floats
     """
