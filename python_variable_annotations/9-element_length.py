df807b9d6233c880396bab2b13c107ede50b1ac0
ilan-deversenne <ilan.deversenne@holbertonstudents.com>
2026-04-09 21:36:11 +0200

    added annotations to element_length

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+Annotations
+"""
+
+import typing
+
+
+def element_length(lst: typing.Iterable[typing.Sequence]) -> typing.List[typing.Tuple[typing.Sequence, int]]:
+    """Return length of elements
+
+    Args:
+        lst (typing.Iterable[typing.Sequence]): Iterable
+
+    Returns:
+        typing.List[typing.Tuple[typing.Sequence, int]]: List
+    """
+
+    return [(i, len(i)) for i in lst]
