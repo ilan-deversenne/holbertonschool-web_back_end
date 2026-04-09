00472c0d977eeb0717043b0383d4d0361ed41135
ilan-deversenne <ilan.deversenne@holbertonstudents.com>
2026-04-09 11:02:01 +0200

    function concat, concatenate two strings

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Function to concatenate two strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates str1 and str2 and returns the result
+    """
+
+    return str1 + str2
