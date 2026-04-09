ee0f9db69845fa5d797c5e6b2a86bfa9ba5f9367
ilan-deversenne <ilan.deversenne@holbertonstudents.com>
2026-04-09 11:22:57 +0200

    function sum_list: sum a list of floats

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Function to sum a list of floats
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    Sums a list of floats
+    """
+
+    return sum(input_list)
