24b64d87b54bdbcbd59a0bd22594b8997628d882
ilan-deversenne <ilan.deversenne@holbertonstudents.com>
2026-04-09 11:13:36 +0200

    function floor, Floor a float to int

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Function to floor a float
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Floor float n and return the result as an integer
+    """
+
+    return int(n)
