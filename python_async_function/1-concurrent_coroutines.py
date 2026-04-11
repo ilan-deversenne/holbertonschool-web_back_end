daad6c8b068a214fe943159bc2062d682f964971
ilan-deversenne <ilan.deversenne@holbertonstudents.com>
2026-04-11 03:06:26 +0200

    fix: added return type

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -5,11 +5,12 @@ Function to create coroutines for wait_random
 """
 
 import asyncio
+import typing
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int):
+async def wait_n(n: int, max_delay: int) -> typing.List:
     """Create n coroutines for wait_random
 
     Args:
@@ -17,7 +18,7 @@ async def wait_n(n: int, max_delay: int):
         max_delay (int): Max delay for wait_random
 
     Returns:
-        list: List with delays
+        typing.List: List with delays
     """
     tasks: list = [asyncio.create_task(wait_random(max_delay)) for i in range(n)]
     return [await task for task in asyncio.as_completed(tasks)]
