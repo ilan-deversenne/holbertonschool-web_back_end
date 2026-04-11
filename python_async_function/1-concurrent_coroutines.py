e5242835d7ea604e1c99022eab65f0e0f1fb6291
ilan-deversenne <ilan.deversenne@holbertonstudents.com>
2026-04-11 03:03:08 +0200

    Function to create coroutines for wait_random

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""
+Function to create coroutines for wait_random
+"""
+
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int):
+    """Create n coroutines for wait_random
+
+    Args:
+        n (int): Number of coroutines
+        max_delay (int): Max delay for wait_random
+
+    Returns:
+        list: List with delays
+    """
+    tasks: list = [asyncio.create_task(wait_random(max_delay)) for i in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
