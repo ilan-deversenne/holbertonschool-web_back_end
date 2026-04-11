c2fb4faa1fc9cb1b016fecc3c34d1533b76b4087
ilan-deversenne <ilan.deversenne@holbertonstudents.com>
2026-04-11 03:08:42 +0200

    fix: removed list for pycodestyle (line height)

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -20,5 +20,5 @@ async def wait_n(n: int, max_delay: int) -> typing.List[float]:
     Returns:
         typing.List: List with delays
     """
-    tasks: list = [asyncio.create_task(wait_random(max_delay)) for i in range(n)]
+    tasks = [asyncio.create_task(wait_random(max_delay)) for i in range(n)]
     return [await task for task in asyncio.as_completed(tasks)]
