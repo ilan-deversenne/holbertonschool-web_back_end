c954f04c25da2d4fb2ab5348b57ae8498b10b5fc
ilan-deversenne <ilan.deversenne@holbertonstudents.com>
2026-04-11 03:07:19 +0200

    fix: I missed float in return type

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -10,7 +10,7 @@ import typing
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> typing.List:
+async def wait_n(n: int, max_delay: int) -> typing.List[float]:
     """Create n coroutines for wait_random
 
     Args:
