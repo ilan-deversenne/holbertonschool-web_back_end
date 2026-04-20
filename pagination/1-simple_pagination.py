d26db10472ac3dd498bb51acef898e5fe2497f9f
ilan-deversenne <ilan.deversenne@holbertonstudents.com>
2026-04-20 17:50:06 +0200

    feat: Copied index_range function in class and added assert to ensure args is int and greather 0 & added get_page function return list of related pages contents

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,55 @@
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def index_range(self, page: int, page_size: int) -> tuple:
+        """Rage index for pagination
+
+        Args:
+            page (int): Page
+            page_size (int): Size page
+
+        Returns:
+            tuple: Tuple of start index & end index
+        """
+
+        assert (type(page_size) is int)
+        assert (type(page) is int)
+        assert (page_size > 0)
+        assert (page > 0)
+
+        return ((page - 1) * page_size, page * page_size)
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Get page
+
+        Args:
+            page (int, optional): Page. Defaults to 1.
+            page_size (int, optional): Page size. Defaults to 10.
+
+        Returns:
+            List[List]: List of related page contents
+        """
+
+        index_range = self.index_range(page, page_size)
+        return self.dataset()[index_range[0]:index_range[1]]
