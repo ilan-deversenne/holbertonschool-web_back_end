29f971e16ba3c45761c491728afb1dbb532602be
ilan-deversenne <ilan.deversenne@holbertonstudents.com>
2026-04-28 14:49:02 +0200

    feat: Created HolbertonCourse with type check & getter and seeter to check type when an element is edited

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,50 @@
+export default class HolbertonCourse {
+    constructor (name, length, students) {
+        if (typeof name !== 'string') {
+            throw "TypeError: Name must be a string";
+        }
+        if (typeof length !== 'number') {
+            throw "TypeError: Length must be a number";
+        }
+        if (typeof students !== 'object' || students.constructor !== Array) {
+            throw "TypeError: Students must be a array";
+        }
+
+        this._name = name;
+        this._length = length;
+        this._students = students;
+    }
+
+    get name() {
+        return this._name;
+    }
+    set name(value) {
+        if (typeof value !== 'string') {
+            throw "TypeError: Name must be a string";
+        }
+
+        this._name = value;
+    }
+
+    get length() {
+        return this._length;
+    }
+    set length(value) {
+        if (typeof length !== 'number') {
+            throw "TypeError: Length must be a number";
+        }
+
+        this._length = value;
+    }
+
+    get students() {
+        return this._students;
+    }
+    set students(value) {
+        if (typeof students !== 'object' || students.constructor !== Array) {
+            throw "TypeError: Students must be a array";
+        }
+
+        this._students = value;
+    }
+}
