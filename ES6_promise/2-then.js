fbf7cc464e314fdf503c408bb892521ef30cd1c0
ilan-deversenne <ilan.deversenne@holbertonstudents.com>
2026-05-04 14:30:38 +0200

    fix: return status and body

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,4 +1,6 @@
 export default function handleResponseFromAPI(promise) {
 
-    return promise.then((val) => console.log('Got a response from the API')).catch((err) => Error());
+    return promise.then((val) => {
+        console.log('Got a response from the API'); return { status: 200, body: 'success' }
+        }).catch((err) => Error());
 }
