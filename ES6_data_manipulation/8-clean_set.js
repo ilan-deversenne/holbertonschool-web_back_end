f7f9bf92d5452184c9707ca3b0cbd58c6d49bfe9
ilan-deversenne <ilan.deversenne@holbertonstudents.com>
2026-05-04 13:45:56 +0200

    fix: return empty string when startString is not given

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,8 @@
 export default function cleanSet(set, startString) {
+    if (!startString || startString == '') return '';
 
     let res = "";
-    set.forEach((val) => { if (val.startsWith(startString) && startString != '') res += `${val.replace(startString, '')}-` });
+    set.forEach((val) => { if (val.startsWith(startString)) res += `${val.replace(startString, '')}-` });
 
     return res.slice(0, -1);
 }
