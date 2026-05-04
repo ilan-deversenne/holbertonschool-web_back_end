59f50756d9d761e0e20f3bfa4a568b4e675933c7
ilan-deversenne <ilan.deversenne@holbertonstudents.com>
2026-05-04 14:00:01 +0200

    feat: updateUniqueItems -> set quantity to 100 if quantity is 1

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,6 @@
+export default function updateUniqueItems(map) {
+    if (!map instanceof Map) throw Error('Cannot process');
+    map.forEach((v, k) => { if (v == 1) map.set(k, 100) });
+
+    return map;
+}
