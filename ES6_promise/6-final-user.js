987e54174cd8d19d2a88b5bc8a30289c62327c06
ilan-deversenne <ilan.deversenne@holbertonstudents.com>
2026-05-05 10:09:54 +0200

    fix: changed response format

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,6 +3,7 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
 
+    let result = [];
     return Promise.allSettled([ signUpUser(firstName, lastName), uploadPhoto(fileName) ])
-        .then((values) => values);
+        .then((values) => { return [ {status: values[0].status, value: values[0].value}, {status: values[0].status, value: values[0].value} ] });
 }
