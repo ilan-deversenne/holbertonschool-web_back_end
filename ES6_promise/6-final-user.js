81e62a7f23b613f8589a3c0809431a07ff75e8c9
ilan-deversenne <ilan.deversenne@holbertonstudents.com>
2026-05-05 10:23:45 +0200

    fix: replaced values index

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,9 +5,9 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
 
     return Promise.allSettled([ signUpUser(firstName, lastName), uploadPhoto(fileName) ])
         .then((values) => {
-            return [
+            console.log( [
                 { status: values[0].status, value: values[0].value },
-                { status: values[0].status, value: values[0].reason ? values[0].reason : values[0].value }
-            ]
+                { status: values[1].status, value: values[1].reason ? values[1].reason : values[1].value }
+            ] )
         });
 }
