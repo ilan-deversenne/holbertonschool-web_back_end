09ee74b8022e1277d3e0e6f334ac842f942a09aa
ilan-deversenne <ilan.deversenne@holbertonstudents.com>
2026-05-11 15:35:39 +0200

    fix: if stdin is TTY log message at end of running

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -7,5 +7,10 @@ const rl = readline.createInterface({
 
 rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
   console.log(`Your name is: ${name}`);
+
+  if (!process.stdin.isTTY) {
+    console.log('This important software is now closing');
+  }
+
   rl.close();
 });
