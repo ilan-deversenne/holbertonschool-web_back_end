08af4585fab5771eefe80cc0818f6e2595248e34
ilan-deversenne <ilan.deversenne@holbertonstudents.com>
2026-05-11 15:17:53 +0200

    fix: Changed code style for linter

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,7 +5,7 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-rl.question("Welcome to Holberton School, what is your name?\n", name => {
+rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
   console.log(`Your name is: ${name}`);
   rl.close();
 });
