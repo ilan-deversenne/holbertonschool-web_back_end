e2c00e3b4397593f91174a5813864b2f3973e6c3
ilan-deversenne <ilan.deversenne@holbertonstudents.com>
2026-05-12 09:44:19 +0200

    fix: eslint and store function before export

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,26 +1,26 @@
-const fs = require('node:fs')
+const fs = require('node:fs');
 
-module.exports = function(filename) {
-    fs.readFile(filename, 'utf8', (err, data) => {
-        if (err) {
-            throw new Error('Cannot load the database')
-            return
-        }
+const countStudents = (filename) => {
+  fs.readFile(filename, 'utf8', (err, data) => {
+    if (err) throw new Error('Cannot load the database');
 
-        let lines = 0
-        let cs = [], swe = []
+    let lines = 0;
+    const cs = []; const
+      swe = [];
 
-        data.split("\n").forEach((line) => {
-            if (line.length < 1) return
+    data.split('\n').forEach((line) => {
+      if (line.length < 1) return;
 
-            lines++
-            if (line.split(',')[3] == 'CS') cs.push(line.split(',')[0])
-            if (line.split(',')[3] == 'SWE') swe.push(line.split(',')[0])
-        })
-        lines--
+      lines += 1;
+      if (line.split(',')[3] === 'CS') cs.push(line.split(',')[0]);
+      if (line.split(',')[3] === 'SWE') swe.push(line.split(',')[0]);
+    });
+    lines -= 1;
 
-        console.log(`Number of students: ${lines}`)
-        console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`)
-        console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`)
-    })
-}
+    console.log(`Number of students: ${lines}`);
+    console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
+    console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
+  });
+};
+
+module.exports = countStudents;
