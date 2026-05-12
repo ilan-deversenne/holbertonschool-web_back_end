5e5bbecc10ba6e44e0f2bd95827a2be2c06597a7
ilan-deversenne <ilan.deversenne@holbertonstudents.com>
2026-05-12 11:51:54 +0200

    fix: using stdin on readble

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,16 +1,11 @@
-const readline = require('node:readline');
+process.stdin.setEncoding('utf8');
+console.log('Welcome to Holberton School, what is your name?');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) process.stdout.write(`Your name is: ${name}`);
 });
 
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  console.log(`Your name is: ${name}`);
-
-  if (!process.stdin.isTTY) {
-    console.log('This important software is now closing');
-  }
-
-  rl.close();
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
 });
