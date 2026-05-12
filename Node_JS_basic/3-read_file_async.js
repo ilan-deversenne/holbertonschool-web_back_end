1473173e52c208a0bda333991c74c6478b43f3d3
ilan-deversenne <ilan.deversenne@holbertonstudents.com>
2026-05-12 12:57:27 +0200

    feat: Count students and swe in a csv file but asynchronously

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,28 @@
+const fs = require('node:fs/promises');
+
+const countStudents = async (filename) => fs.readFile(
+  filename,
+  { encoding: 'utf8', flag: 'r' },
+).then((data) => {
+  let lines = 0;
+  const cs = []; const
+    swe = [];
+
+  data.split('\n').forEach((line) => {
+    if (line.length < 1) return;
+    if (line.split(',')[0].length < 1) return;
+
+    lines += 1;
+
+    if (line.split(',')[3] === 'CS') cs.push(line.split(',')[0]);
+    if (line.split(',')[3] === 'SWE') swe.push(line.split(',')[0]);
+  });
+
+  if (lines > 0) lines -= 1;
+
+  console.log(`Number of students: ${lines}`);
+  console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
+  console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
+});
+
+module.exports = countStudents;
