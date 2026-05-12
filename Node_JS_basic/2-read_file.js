cf6d02be199bfd9f92422bd187958e403ce31bcb
ilan-deversenne <ilan.deversenne@holbertonstudents.com>
2026-05-12 10:48:52 +0200

    fix: using try

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,27 +1,32 @@
 const fs = require('node:fs');
 
 const countStudents = (filename) => {
-  fs.readFile(filename, 'utf8', (err, data) => {
-    if (err) throw new Error('Cannot load the database');
+  let data;
+  try {
+    data = fs.readFileSync(filename, 'utf8');
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
 
-    let lines = 0;
-    const cs = []; const
-      swe = [];
+  let lines = 0;
+  const cs = []; const
+    swe = [];
 
-    data.split('\n').forEach((line) => {
-      if (line.length < 1) return;
-      if (line.split(',')[0].length < 1) return;
+  data.split('\n').forEach((line) => {
+    if (line.length < 1) return;
+    if (line.split(',')[0].length < 1) return;
 
-      lines += 1;
-      if (line.split(',')[3] === 'CS') cs.push(line.split(',')[0]);
-      if (line.split(',')[3] === 'SWE') swe.push(line.split(',')[0]);
-    });
-    if (lines > 0) lines -= 1;
+    lines += 1;
 
-    console.log(`Number of students: ${lines}`);
-    console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
-    console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
+    if (line.split(',')[3] === 'CS') cs.push(line.split(',')[0]);
+    if (line.split(',')[3] === 'SWE') swe.push(line.split(',')[0]);
   });
+
+  if (lines > 0) lines -= 1;
+
+  console.log(`Number of students: ${lines}`);
+  console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
+  console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
 };
 
 module.exports = countStudents;
